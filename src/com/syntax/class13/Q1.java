38c2153594a8e061d741839eec48cbbe01b3b4f8
ktemiz <online4kzm@gmail.com>
2020-03-23 19:42:15 -0400

    regular commit

---
+++ src/com/syntax/class13/Q1.java
@@ -0,0 +1,22 @@
+package com.syntax.class13;
+
+public class Q1 {
+	
+	public static void main(String[] args) {
+		
+		int i =5;
+		int j =9;
+		
+		if(i!=j) {
+			 i=i;
+			 
+			 System.out.println(i);
+			 
+			 i=j;
+			 System.out.println(j);
+			
+			
+		}
+	}
+
+}
---
+++ src/com/syntax/class13/Q2.java
@@ -0,0 +1,22 @@
+package com.syntax.class13;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+	 
+		int a=0;
+		int b=1;
+		int c=0;
+		
+		for (int i=0; i<=10; i++) {
+			
+			a=+i;
+			c=c+b;
+			System.out.println(c);
+		}
+	}
+
+}
+
+
+//0+1 1+
