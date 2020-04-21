02427717082e70138244836d41a0452669b0fd8e
ktemiz <online4kzm@gmail.com>
2020-04-20 23:33:04 -0400

    repl updating

--- src/ReplIt_161to220/Rep186.java
+++ src/ReplIt_161to220/Rep186.java
@@ -2,32 +2,25 @@ package ReplIt_161to220;
 
 import java.util.*;
 
-public class Rep186{
+public class Rep186 {
 
-public static void main (String[] args)
-{		
-    int i =0;
-    int num =0;
-    //Empty String
-    int  primeNumbers =0;
+	public static void main(String[] args) {
 
-    for (i = 1; i <= 100; i++)         
-    { 		  	  
-       int counter=0; 	  
-       for(num =i; num>=1; num--)
-	  {
-          if(i%num==0)
-	     {
-		counter = counter + 1;
-	     }
-	  }
-	  if (counter ==2)
-	  {
-	     //Appended the Prime number to the String
-	    primeNumbers=i;
-	  }	
-    }	
-    System.out.println("Prime numbers from 1 to 100 are :");
-    System.out.println(primeNumbers);
-}
+		LinkedList<Integer> prime = new LinkedList<>();
+		
+		for (int i = 2; i <= 100; i++) {
+			prime.add(i);
+		}
+
+		for (int a = 2; a <= 100; a++) {
+			for (int j = 2; j < a; j++) {
+				if (a % j == 0) {
+					prime.remove((Integer) a);
+					break;
+				}
+
+			}
+		}
+		System.out.println(prime);
+	}
 }
